54cf2f07abc5dbc0c6e542a20e139f3753c555fc
Frikky <frikky@shuffler.io>
2024-10-23 00:05:48 +0200

    Some more command generation paths

--- translate.go
+++ translate.go
@@ -56,14 +56,16 @@ func TranslateToShuffle(cacao *Cacao) shuffle.Workflow {
 	actions := make(map[string]Action)
 	for key, value := range cacao.Workflow {
 		newAction := shuffle.Action{
-			AppName: "shuffle tools",
+			AppName: "Shuffle Tools",
 			AppVersion: "1.2.0",
-			Name: value.Name,
+			Label: value.Name,
 			Description: value.Description,
 		}
 
+		newActionID := ""
 		if strings.Contains(key, "--") {
-			newAction.ID = strings.Split(key, "--")[1]
+			newActionID = strings.Split(key, "--")[1]
+			newAction.ID = newActionID
 		} else {
 			log.Printf("\n\n[ERROR] No ID found in key: %s\n\n", key)
 			continue
@@ -107,17 +109,35 @@ func TranslateToShuffle(cacao *Cacao) shuffle.Workflow {
 			}
 		} else if strings.HasPrefix(key, "action--") {
 			actions[key] = value
+
+			newAction.Label = value.Name
 			configureAction = true
 		} else {
 			log.Printf("\n\n[ERROR] Unknown key: %s\n\n", key)
 			continue
 		}
 
+		appended := false
 		if configureAction {
 			log.Printf("[DEBUG] Configuring action: %s", key)
 
-			for _, command := range value.Commands {
+			if len(value.Targets) > 0 {
+				log.Printf("\n\n[WARNING] Adding target configs to action: %s\n\n", key)
+			}
+
+			nextActionID := ""
+			for commandCount, command := range value.Commands {
+				// From the top just in case it's not overridden
+				//if len(newActionID) > 0 { 
+				//	newAction.ID = newActionID
+				//}
+
+				if len(value.Commands) > 1 {
+					newAction.Label = fmt.Sprintf("%d", commandCount+1)+"-"+value.Name
+				}
+
 				if command.Type == "http-api" || command.Type == "http" {
+
 					newAction.AppName = "http"
 					newAction.AppVersion = "1.4.0"
 
@@ -138,7 +158,11 @@ func TranslateToShuffle(cacao *Cacao) shuffle.Workflow {
 
 					headerString := ""
 					for headerKey, headerValue := range command.Headers {
-						headerString += fmt.Sprintf("%s: %s\n", headerKey, headerValue)
+						if len(headerValue) == 0 {
+							continue
+						}
+
+						headerString += fmt.Sprintf("%s: %s\n", headerKey, headerValue[0])
 					}
 
 					newAction.Name = method
@@ -146,37 +170,84 @@ func TranslateToShuffle(cacao *Cacao) shuffle.Workflow {
 						shuffle.WorkflowAppActionParameter{
 							Name: "url",
 							Value: url,
+
+							Required: true,
 						},
 						shuffle.WorkflowAppActionParameter{
 							Name: "headers",
 							Value: headerString,
+
+							Required: false,
 						},
 						shuffle.WorkflowAppActionParameter{
-							Name: "body",
-							Value: command.Content,
+							Name: "verify",
+							Value: "false",
+
+							Required: false,
+						},
+						shuffle.WorkflowAppActionParameter{
+							Name: "timeout",
+							Value: "10",
+
+							Required: false,
 						},
 					}
 
+					if len(command.Content) > 0 {
+						newAction.Parameters = append(newAction.Parameters, shuffle.WorkflowAppActionParameter{
+							Name: "body",
+							Value: command.Content,
+
+							Required: false,
+						})
+					}
+				}
+
+				if len(value.Commands) > 1 {
+					appended = true
+
+
+					if commandCount != 0 && commandCount != len(value.Commands)-1 {
+						if len(nextActionID) == 0 {
+							newAction.ID = uuid.New().String()
+						} else {
+							newAction.ID = nextActionID
+						}
+					}
+
+					nextActionID = uuid.New().String()
+					shuffleWorkflow.Branches = append(shuffleWorkflow.Branches, shuffle.Branch{
+						ID: uuid.New().String(),
+						SourceID: newAction.ID,
+						DestinationID: nextActionID,
+					})
+
+
+					shuffleWorkflow.Actions = append(shuffleWorkflow.Actions, newAction)
+					continue
 				}
 			}
 
-
-			if len(value.Targets) > 0 {
-				log.Printf("\n\n[WARNING] Adding target configs to action: %s\n\n", key)
+			if appended { 
+				continue
 			}
-
 		} else {
 			newAction.Name = "repeat_back_to_me"
-			if strings.Contains(key, "--") {
-				newAction.Label = strings.Split(key, "--")[1]
-			} else {
-				newAction.Label = key
+
+			if len(newAction.Label) == 0 {
+				if strings.Contains(key, "--") {
+					newAction.Label = strings.Split(key, "--")[1]
+				} else {
+					newAction.Label = key
+				}
 			}
 
 			newAction.Parameters = []shuffle.WorkflowAppActionParameter{
 				shuffle.WorkflowAppActionParameter{
 					Name: "call",
 					Value: key,
+
+					Required: true,
 				},
 			}
 		}
